1be73a7789e077a128be0c4fe2a4474f79de9f46
TUL-ADMIN <897-lkhj12@MacBook-Pro-de-TUL-ADMIN-93.local>
2022-02-18 23:48:02 -0500

    add clients

--- scripts/afterInstall.sh
+++ scripts/afterInstall.sh
@@ -4,4 +4,3 @@ cd /var/www/enertec && sudo php artisan config:cache
 cd /var/www/enertec && sudo php artisan route:cache
 cd /var/www/enertec && sudo php artisan migrate
 cd /var/www/enertec && sudo php artisan schedule:work&
-cd /var/www/enertec && sudo laravel-echo-server start&
