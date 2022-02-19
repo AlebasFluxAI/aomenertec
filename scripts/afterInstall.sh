ddfa132e8eba2cd1f66a74a7c50da4c52c6834a8
TUL-ADMIN <897-lkhj12@MacBook-Pro-de-TUL-ADMIN-93.local>
2022-02-18 23:14:07 -0500

    add scheduler

--- scripts/afterInstall.sh
+++ scripts/afterInstall.sh
@@ -4,4 +4,3 @@ cd /var/www/enertec && sudo composer install
 cd /var/www/enertec && sudo php artisan config:cache
 cd /var/www/enertec && sudo php artisan route:cache
 cd /var/www/enertec && sudo php artisan migrate
-cd /var/www/enertec && sudo laravel-echo-server start
