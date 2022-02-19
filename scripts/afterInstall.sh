9ab71fdd730fec1b9d0c4fe2aa646d1278e670d9
TUL-ADMIN <897-lkhj12@MacBook-Pro-de-TUL-ADMIN-93.local>
2022-02-19 13:51:53 -0500

    revision

--- scripts/afterInstall.sh
+++ scripts/afterInstall.sh
@@ -3,5 +3,7 @@
 cd /var/www/enertec && sudo php artisan config:cache
 cd /var/www/enertec && sudo php artisan route:cache
 cd /var/www/enertec && sudo php artisan migrate
-sudo /var/www/enertec laravel-echo-server start > /dev/null 2> /dev/null < /dev/null &
+cd /var/www/enertec && sudo laravel-echo-server start > /dev/null 2> /dev/null < /dev/null &
+sudo pkill -9 -f queue:work
+cd /var/www/enertec && sudo php artisan queue:work  > /dev/null 2> /dev/null < /dev/null &
 
