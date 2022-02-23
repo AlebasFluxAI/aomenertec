2d70ad0ebd7efe1d8f7e5abc690750b11932c6d0
TUL-ADMIN <897-lkhj12@MacBook-Pro-de-TUL-ADMIN-93.local>
2022-02-23 08:45:40 -0500

    add table component

--- resources/views/livewire/administrar/v1/equipment/index-equipment.blade.php
+++ resources/views/livewire/administrar/v1/equipment/index-equipment.blade.php
@@ -20,24 +20,6 @@
 
                   ]
           ])
-@include("partials.v1.table.primary-table",[
-           "table_headers"=>["ID"=>"id",
-                             "Nombre"=>"name",
-                             "Descripcion"=>"description",
-                             "Tipo de equipo"=>"equipment_type.type",
 
-
-            ],
-             "table_actions"=>[
-                                            "edit"=>"editEquipment",
-                                            "delete"=>"deleteEquipment"
-                                         ],
-                                            /* Le dice al componente tabla las acciones que tendra la columna de acciones en la tabla [
-                                            _edit_button=>{ruta para redireccionar a edicion}
-                                            _delete_button => {boton de borrado, siempre tomando como identificador la primera colunma de la tabla - ID}
-                                              ]*/
-           "table_rows"=>$equipments
-
-       ])
 
 </div>
