0b4cbc77c2d8c41a188e8fa90b246783bd7eaf25
TUL-ADMIN <897-lkhj12@192.168.0.9>
2022-03-06 18:27:36 -0500

    rebase

--- scripts/beforeInstall.sh
+++ scripts/beforeInstall.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-cd /var/www/enertec && sudo composer install --verbose --prefer-dist --no-progress --no-interaction --no-dev --optimize-autoloader --no-suggest --apcu-autoloader
+cd /var/www/enertec && sudo composer install --ignore-platform-req=ext-rdkafka --verbose --prefer-dist --no-progress --no-interaction --no-dev --optimize-autoloader --no-suggest --apcu-autoloader
 
