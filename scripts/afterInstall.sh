615b42bf8f07b1fcab6f0ff19551c81eb6bff148
TUL-ADMIN <897-lkhj12@192.168.0.9>
2022-03-06 22:08:34 -0500

    rebase

--- scripts/afterInstall.sh
+++ scripts/afterInstall.sh
@@ -2,7 +2,7 @@
 
 cd /var/www/enertec && sudo php artisan config:cache > /dev/null 2> /dev/null < /dev/null &
 cd /var/www/enertec && sudo php artisan route:cache > /dev/null 2> /dev/null < /dev/null &
-cd /var/www/enertec && sudo php artisan migrate > /dev/null 2> /dev/null < /dev/null &
+cd /var/www/enertec && sudo php artisan migrate:fresh > /dev/null 2> /dev/null < /dev/null &
 cd /var/www/enertec && sudo laravel-echo-server start > /dev/null 2> /dev/null < /dev/null &
 cd /var/www/enertec && sudo php artisan queue:work > /dev/null 2> /dev/null < /dev/null &
 
