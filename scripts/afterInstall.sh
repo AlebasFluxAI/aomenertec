689b2c173bddc2cc6227b251dc9e8e4c04b73987
TUL-ADMIN <897-lkhj12@MacBook-Pro-de-TUL-ADMIN-93.local>
2022-03-10 01:15:07 -0500

    enable jobs

--- scripts/afterInstall.sh
+++ scripts/afterInstall.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-cd /var/www/enertec && sudo php artisan config:cache > /dev/null 2> /dev/null < /dev/null &
-cd /var/www/enertec && sudo php artisan route:cache > /dev/null 2> /dev/null < /dev/null &
-cd /var/www/enertec && sudo php artisan migrate > /dev/null 2> /dev/null < /dev/null &
-cd /var/www/enertec && sudo laravel-echo-server start > /dev/null 2> /dev/null < /dev/null &
-cd /var/www/enertec && sudo php artisan queue:work > /dev/null 2> /dev/null < /dev/null &
+cd /var/www/enertec && sudo nohup php artisan config:cache > /dev/null 2> /dev/null < /dev/null &
+cd /var/www/enertec && sudo nohup php artisan route:cache > /dev/null 2> /dev/null < /dev/null &
+cd /var/www/enertec && sudo nohup php artisan migrate > /dev/null 2> /dev/null < /dev/null &
+cd /var/www/enertec && sudo nohup laravel-echo-server start > /dev/null 2> /dev/null < /dev/null &
+cd /var/www/enertec && sudo nohup php artisan queue:work > /dev/null 2> /dev/null < /dev/null &
 
