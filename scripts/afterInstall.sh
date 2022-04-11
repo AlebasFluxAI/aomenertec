635a06a37f2fd60f06f0eeaeb9926c3839867d07
Wilder Herrera SAC <wherrera@soytul.com>
2022-04-11 01:49:03 -0500

    release

--- scripts/afterInstall.sh
+++ scripts/afterInstall.sh
@@ -3,3 +3,4 @@
 cd /var/www/enertec && sudo nohup php artisan config:cache > /dev/null 2> /dev/null < /dev/null &
 cd /var/www/enertec && sudo nohup php artisan route:cache > /dev/null 2> /dev/null < /dev/null &
 cd /var/www/enertec && sudo nohup php artisan migrate > /dev/null 2> /dev/null < /dev/null &
+
