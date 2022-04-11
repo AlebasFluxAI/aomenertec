9b6536134a191229a7facb1095942f9b6bf92e7e
Wilder Herrera SAC <wherrera@soytul.com>
2022-04-11 01:39:06 -0500

    release

--- scripts/afterInstall.sh
+++ scripts/afterInstall.sh
@@ -3,8 +3,3 @@
 cd /var/www/enertec && sudo nohup php artisan config:cache > /dev/null 2> /dev/null < /dev/null &
 cd /var/www/enertec && sudo nohup php artisan route:cache > /dev/null 2> /dev/null < /dev/null &
 cd /var/www/enertec && sudo nohup php artisan migrate > /dev/null 2> /dev/null < /dev/null &
-cd /var/www/enertec && sudo nohup laravel-echo-server start > /dev/null 2> /dev/null < /dev/null &
-cd /var/www/enertec && sudo nohup php artisan queue:listen > /dev/null 2> /dev/null < /dev/null &
-cd /var/www/enertec/script && sudo nohup python3 receiveMqttEvent.py > /dev/null 2> /dev/null < /dev/null &
-cd /var/www/enertec/script && sudo nohup python3 receiveMqttRealTimeEvent.py.py > /dev/null 2> /dev/null < /dev/null &
-
