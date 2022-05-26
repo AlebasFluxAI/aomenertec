c379e6f8a8e3e5ce99b8b8b71aac0be59cddaf91
Wilder Herrera SAC <wherrera@soytul.com>
2022-05-26 09:57:58 -0500

    zone

--- resources/views/livewire/v1/admin/equipment/index-equipment.blade.php
+++ resources/views/livewire/v1/admin/equipment/index-equipment.blade.php
@@ -48,7 +48,7 @@
                                [
                                    "col_name" =>"Asignado",
                                    "col_data" =>"assigned",
-                                   "col_filter"=>true,
+                                   "col_filter"=>false,
                                    "col_type"=>\App\Http\Resources\V1\ColTypeEnum::COL_TYPE_BOOLEAN_INVERSE
                                ],
                         ],
