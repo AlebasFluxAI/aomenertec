55bd8e387bc32b75b06fe1b6c591c943e83e8a16
Wilder Herrera SAC <wherrera@soytul.com>
2022-06-13 15:28:53 -0500

    revision

--- resources/views/livewire/v1/admin/user/technician/profile-technician.blade.php
+++ resources/views/livewire/v1/admin/user/technician/profile-technician.blade.php
@@ -122,7 +122,7 @@
        ["nav_options"=>[
                   ["button_align"=>"right",
                   "click_action"=>"",
-                  "button_content"=>"Cerrar cesion",
+                  "button_content"=>"Cerrar sesión",
                   "button_icon"=>"fa-solid fa-right-from-bracket",
                   "target_route"=>"logout",
                   ],
