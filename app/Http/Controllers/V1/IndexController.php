c93168d1c503c06b796c92daecc5418302195d05
Wilder Herrera SAC <wherrera@soytul.com>
2022-06-30 23:23:12 -0500

    add password reset

--- app/Http/Controllers/V1/IndexController.php
+++ app/Http/Controllers/V1/IndexController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\V1;
 use App\Http\Resources\V1\Subdomain;
 use Illuminate\Http\Request;
 use App\Models\V1\User;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 class IndexController extends Controller
@@ -12,11 +13,21 @@ class IndexController extends Controller
     public function index()
     {
 
+        if (Auth::user()) {
+            return redirect()->route("administrar.v1.perfil");
+        }
         $subdomain = Route::input("subdomain");
+        
         return match ($subdomain) {
             Subdomain::SUBDOMAIN_AOM => Subdomain::SUBDOMAIN_AOM > view('auth.login'),
             Subdomain::SUBDOMAIN_COENERGIA => view("auth.coenergia_login"),
             default => view('auth.login'),
         };
+    }
+
+    public function forgotPassword()
+    {
+        return view('auth.passwords.email');
+
     }
 }
--- resources/views/auth/reset-password.blade.php
+++ resources/views/auth/reset-password.blade.php
@@ -2,10 +2,16 @@
 <x-slot name="logo">
     <x-jet-authentication-card-logo/>
 </x-slot>
-@include("layouts.menu.v1.header_menu")
+@include("layouts.menu.v1.header_menu_password")
 <x-jet-validation-errors class="mb-4"/>
-<div class="col-6 offset-3 mt-8">
-    <form method="POST" action="{{ route('password.update') }}">
+<div class="col-6 offset-3 mt-8 bg-light p-5">
+
+    <h3>Reiniciar contraseña</h3>
+    <hr>
+
+    <br>
+    <form method="POST"
+          action="{{ route('password.update',["subdomain"=>\Illuminate\Support\Facades\Route::input("subdomain")??"enertec"]) }}">
         @csrf
 
         <input type="hidden" name="token" value="{{ $request->route('token') }}">
@@ -17,20 +23,20 @@
         </div>
 
         <div class="mt-4">
-            <x-jet-label for="password" value="{{ __('Password') }}"/>
+            <x-jet-label for="password" value="Nueva contraseña"/>
             <x-jet-input id="password" class="block mt-1 w-full" type="password" name="password" required
                          autocomplete="new-password"/>
         </div>
 
         <div class="mt-4">
-            <x-jet-label for="password_confirmation" value="{{ __('Confirm Password') }}"/>
+            <x-jet-label for="password_confirmation" value="Confirmar contraseña"/>
             <x-jet-input id="password_confirmation" class="block mt-1 w-full" type="password"
                          name="password_confirmation" required autocomplete="new-password"/>
         </div>
 
         <div class="flex items-center justify-end mt-4">
             <x-jet-button>
-                {{ __('Reset Password') }}
+                Confirmar
             </x-jet-button>
         </div>
     </form>
