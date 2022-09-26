b3c8be32bdba526281fbbe677951734317902a05
Wilder Herrera SAC <wherrera@soytul.com>
2022-09-26 09:58:51 -0500

    agregar link de recuperacion para subdominios

--- scripts/afterInstall.sh
+++ scripts/afterInstall.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd /var/www/enertec && sudo nohup php artisan config:cache > /dev/null 2> /dev/null < /dev/null &
-cd /var/www/enertec && sudo nohup php artisan route:cache > /dev/null 2> /dev/null < /dev/null &
-cd /var/www/enertec && sudo nohup php artisan migrate > /dev/null 2> /dev/null < /dev/null &
+cd /var/www/enertec && sudo nohup php artisan config:cache
+cd /var/www/enertec && sudo nohup php artisan route:cache
+cd /var/www/enertec && sudo nohup php artisan migrate
 
