ee815d7cb51e58dd3ac395de27dfd4f546213f5e
Wilder Herrera <wherrera@soytul.com>
2022-10-21 23:42:58 -0500

    agregar modulo de alertas a cliente

---
+++ app/Http/Livewire/V1/Admin/Client/ClientAlertIndex.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Livewire\V1\Admin\Client;
+
+use App\Http\Services\V1\Admin\Client\ClientAlertIndexService;
+use App\Http\Services\V1\Admin\Client\IndexClientService;
+use App\Http\Services\V1\Admin\Equipment\EquipmentIndexService;
+use App\Models\Traits\FilterTrait;
+use App\Models\V1\Client;
+use App\Models\V1\Equipment;
+use Livewire\Component;
+use Livewire\WithPagination;
+
+class ClientAlertIndex extends Component
+{
+    use WithPagination;
+    use FilterTrait;
+
+    public $model;
+    private $clientAlertIndexService;
+
+    public function __construct($id = null)
+    {
+        $this->clientAlertIndexService = ClientAlertIndexService::getInstance();
+        parent::__construct($id);
+    }
+
+    public function mount(Client $client)
+    {
+        $this->clientAlertIndexService->mount($this, $client);
+    }
+
+    public function render()
+    {
+        return view('livewire.v1.admin.client.client-alert-index', [
+            "data" => $this->getData()
+        ])->extends('layouts.v1.app');
+    }
+
+    public function getData()
+    {
+        return $this->clientAlertIndexService->getData($this);
+    }
+}
---
+++ resources/views/livewire/v1/admin/client/client-alert-index.blade.php
@@ -0,0 +1,67 @@
+@if($view_header??true)
+    <div class="login">
+        @section("header")
+            {{--extended app.blade--}}
+
+        @endsection
+
+
+        @include("partials.v1.title",[
+              "second_title"=>"de alertas cliente $model->name",
+              "first_title"=>"Listado"
+          ])
+        @endif
+
+
+        @include("partials.v2.table.primary-table",[
+                "class_container"=>$table_class_container??null,
+               "table_pageable"=>$table_pageable??true,
+               "table_headers"=>[
+              [
+                   "col_name" =>"ID",
+                   "col_data" =>"id",
+                   "col_filter"=>true
+               ],
+               [
+                   "col_name" =>"Variable",
+                   "col_data" =>"name",
+                   "col_filter"=>true
+               ],
+               [
+                   "col_name" =>"Limite min",
+                   "col_data" =>"clientAlertConfiguration.min_aler",
+                   "col_filter"=>true
+               ],
+               [
+                   "col_name" =>"Limite max",
+                   "col_data" =>"clientAlertConfiguration.max_alert",
+                   "col_filter"=>true
+               ],
+               [
+                   "col_name" =>"Valor alerta",
+                   "col_data" =>"value",
+                   "col_filter"=>true
+               ],
+               [
+                   "col_name" =>"Tipo",
+                   "col_data" =>"type",
+                   "col_filter"=>true
+               ],
+               [
+                    "col_name" =>"Actualizacion limites",
+                    "col_data" =>"clientAlertConfiguration.updated_at",
+                    "col_filter"=>false
+               ],
+               [
+                    "col_name" =>"Fecha",
+                    "col_data" =>"created_at",
+                    "col_filter"=>false
+               ],
+
+                ],
+               "table_actions"=>[],
+               "table_rows"=>$data
+           ])
+        @if($view_header??true)
+    </div>
+@endif
