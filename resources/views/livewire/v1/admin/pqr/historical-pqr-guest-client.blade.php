97e259b83e481d861bdc663be074449cbd7cdc9c
Wilder Herrera <wherrera@soytul.com>
2022-10-28 16:21:09 -0500

    agregar vista de historico de pqr en guest

--- resources/views/livewire/v1/admin/pqr/historical-pqr-guest-client.blade.php
+++ resources/views/livewire/v1/admin/pqr/historical-pqr-guest-client.blade.php
@@ -1,48 +1,81 @@
-<div>
-    <div style="background-color: #f2f2f2;padding: 15px;border-radius: 15px">
+<div class="login" style=" border-color: #f2f2f2;border-width: 2px;padding: 15px;border-radius: 15px">
+
+    @section("header")
+        {{--extended app.blade--}}
+        @auth
+        @else
+            @include("layouts.menu.v1.header_menu_password")
+        @endauth
+    @endsection
 
 
-        @include("layouts.menu.v1.header_menu_password")
 
-        <div class="section-title my-5">
-            <h3 class="text-center p3"><b><span class="fas fa-ticket"></span> Seguimiento de peticiones para
-                    clientes</b>
-            </h3>
-        </div>
-        <div class="contenedor-grande">
-            <form wire:submit.prevent="submitForm" id="formulario" class="needs-validation" role="form">
-                @include("partials.v1.divider_title",[
-                                           "title"=>"codigo de PQR"
-                                   ]
-                                  )
+    @include("partials.v1.title",[
+          "second_title"=>"",
+          "first_title"=>"Historico PQR ".$model->code,
+      ])
+
+    @auth
+        @include("partials.v1.table_nav",
+                [
+                 "mt"=>2,
+                 "nav_options"=>[
+                        [
+                        "permission"=>[\App\Http\Resources\V1\Permissions::PQR_SHOW],
+                        "button_align"=>"right",
+                        "click_action"=>"",
+                        "button_content"=>"Ver listado",
+                        "button_icon"=>"fa-solid fa-list",
+                        "target_route"=>"administrar.v1.peticiones.listado",
+                        ],
+
+                    ]
+                ])
+    @endauth
+    @include("partials.v1.primary-card",[
+               'card_title'=>"Peticion de cliente: ",
+               'card_subtitle'=>$model->code,
+               'card_body'=>[
+                               [
+                                      "name"=>"Asunto",
+                                      "value"=>$model->subject
+                               ] ,
+
+                            ]
+           ])
 
 
-                @include("partials.v1.form.form_input_icon",[
-                        "input_label"=>"Ingrese el codigo de cliente",
-                        "input_model"=>"client_code",
-                        "icon_class"=>"fas fa-user",
-                        "placeholder"=>"Ingresa tu codigo de cliente",
-                        "col_with"=>6,
-                        "input_type"=>"text",
-               ])
-
-                @include("partials.v1.form.form_input_icon",[
-                      "input_label"=>"Ingrese el codigo de Pqr",
-                      "input_model"=>"pqr_code",
-                      "icon_class"=>"fas fa-user",
-                      "placeholder"=>"Ingresa el codigo de Pqr",
-                      "col_with"=>6,
-                      "input_type"=>"text",
-             ])
-
-
-                @include("partials.v1.form.form_submit_button",[
-                                      "button_align"=>"right" ,
-                                      "button_content"=>"Enviar petición"
+    <div
+        style="overflow-y: scroll;height:500px;border-color: #f2f2f2;border-width: 3px;padding: 5px;border-radius: 15px">
+        @if(!count($model->messages))
+            <div class="text-center mt-5">
+                <i style="font-size: 5rem" class="fa-solid fa-inbox empty-table"></i>
+                <p class="empty-table-text">{{$table_empty_text??"No existen Mensajes para esta petición"}}</p>
+            </div>
+        @else
+            @foreach($model->messages as$message)
+                <ul id="{{$message->sender_type==\App\Models\V1\PqrMessage::SENDER_TYPE_USER?"message-box-left":"message-box-right"}}">
+                    <p class="text-right">{{\Carbon\Carbon::parse($message->created_at)->format('d/m/Y H:i:s')}} </p>
+                    <p class="text-right">Enviado por {{$message->sender()?$message->sender()->name:''}} </p>
+                    <hr class="mx-5 my-2">
+                    <ul style="margin-left: 10px">
+                        {{$message->message}}
+                    </ul>
+                    <hr class="mx-5 my-2">
+                    @if($message->attach and $message->attach->name!="no_found.png")
+                        @include("partials.v1.image",[
+                            "image_url"=>$message->attach->url
                           ])
-
-            </form>
-
-        </div>
+                    @endif
+                </ul>
+            @endforeach
+        @endif
     </div>
+    <div class="m-5">
+
+        <a href="{{ url()->previous() }}" class="btn btn-default">Volver</a>
+    </div>
+
+
 </div>
+
