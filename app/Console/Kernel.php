3f2e77a33e6109b9e67575da7e0ef13daf36bf35
LAPTOP-C2R57CV8\EnertecLatinoamerica <s.fuentes@enerteclatam.com>
2022-11-02 15:36:40 -0500

    test re edit data

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -32,8 +32,8 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         ////unpack data
-        $schedule->command(UpdateTimestampDataConsumption::class)->everyTwoMinutes()->withoutOverlapping();
-        $schedule->command(UpdateDataConsumption::class)->everyTwoMinutes()->withoutOverlapping();
+        //$schedule->command(UpdateTimestampDataConsumption::class)->everyTwoMinutes()->withoutOverlapping();
+        //$schedule->command(UpdateDataConsumption::class)->everyTwoMinutes()->withoutOverlapping();
         $schedule->command(SetTimestamp::class)->twiceDailyAt(10, 22, 3);
         $schedule->command(SetTimestamp::class)->twiceDailyAt(4, 16, 3);
 
