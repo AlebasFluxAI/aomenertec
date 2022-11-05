ac6bed67df28987f66770261edce0068d99557d8
Wilder Herrera <wherrera@soytul.com>
2022-11-05 09:01:41 -0500

    reparar colas fallando por array en configuracion

--- app/Observers/V1/Change/ChangeObserver.php
+++ app/Observers/V1/Change/ChangeObserver.php
@@ -12,21 +12,21 @@ class ChangeObserver
     public function created(Model $model)
     {
         if (Auth::check()) {
-            dispatch(new ChangeRegisterJob($model, $model->getOriginal(), $model->getAttributes(), Change::CHANGE_TYPE_CREATED, Auth::user(), $model->getChanges()))->onQueue("default");
+            dispatch(new ChangeRegisterJob($model, $model->getOriginal(), $model->getAttributes(), Change::CHANGE_TYPE_CREATED, Auth::user(), $model->getChanges()));
         }
     }
 
     public function updated(Model $model)
     {
         if (Auth::check()) {
-            dispatch(new ChangeRegisterJob($model, $model->getOriginal(), $model->getAttributes(), Change::CHANGE_TYPE_UPDATED, Auth::user(), $model->getChanges()))->onQueue("default");
+            dispatch(new ChangeRegisterJob($model, $model->getOriginal(), $model->getAttributes(), Change::CHANGE_TYPE_UPDATED, Auth::user(), $model->getChanges()));
         }
     }
 
     public function deleted(Model $model)
     {
         if (Auth::check()) {
-            dispatch(new ChangeRegisterJob($model, $model->getOriginal(), $model->getAttributes(), Change::CHANGE_TYPE_DELETED, Auth::user(), $model->getChanges()))->onQueue("default");
+            dispatch(new ChangeRegisterJob($model, $model->getOriginal(), $model->getAttributes(), Change::CHANGE_TYPE_DELETED, Auth::user(), $model->getChanges()));
         }
     }
 }
