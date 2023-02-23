50116d84fcfa093a798987a4d354abb2fd4245ed
Wilder Herrera <wherrera@soytul.com>
2023-02-22 20:59:48 -0500

    agregar simulador de tarifa

--- app/Http/Resources/V1/Formatter.php
+++ app/Http/Resources/V1/Formatter.php
@@ -9,11 +9,11 @@ class Formatter
         return (number_format($money, 0)) . " " . $currency;
     }
 
-    public static function numberFormat($money)
+    public static function numberFormat($money, $decimals = 0)
     {
         if (!$money) {
             return 0;
         }
-        return (number_format($money, 0));
+        return (number_format($money, $decimals));
     }
 }
