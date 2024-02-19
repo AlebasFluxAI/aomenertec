11af61bd55a23451ee068166724bf5a6ad4e11c0
SneiderFuentes <s.fuentes@enerteclatam.com>
2024-02-19 16:58:01 -0500

    update unpackdataJob

--- app/Console/ConsumerCommand.php
+++ app/Console/ConsumerCommand.php
@@ -35,24 +35,11 @@ class ConsumerCommand extends Command
     public function handle()
     {
         $mqtt = MQTT::connection('default', 'client_consumer_princi');
-        $mqtt->subscribe('mc/real_time/v1', function (string $topic, string $message) {
-            dispatch(new PushRealTimeMicrocontrollerDataJob($message))->onQueue('default');
-        }, 1);
-        $mqtt->subscribe('mc/data/v1', function (string $topic, string $message) {
-            dispatch(new SaveMicrocontrollerDataJob($message, false))->onQueue('spot');
-        }, 1);
-        $mqtt->subscribe('mc/alert/v1', function (string $topic, string $message) {
-            // echo "msj aLERTA = ".$message."\n";
-            dispatch(new SaveMicrocontrollerDataJob($message, true))->onQueue('spot');
-            dispatch(new SaveAlertDataJob($message))->onQueue('default');
-        }, 2);
 
-        $mqtt->subscribe('mc/real_time', function (string $topic, string $message) {
-
-            dispatch(new PushRealTimeMicrocontrollerDataJob($message))->onQueue('default');
-        }, 0);
         $mqtt->subscribe('v1/mc/alert', function (string $topic, string $message) {
-            //echo "msj ALERTA test= ".$message."\n";
+            $pack= base64_encode($message);
+            dispatch(new SaveMicrocontrollerDataJob($pack, true))->onQueue('spot');
+            dispatch(new SaveAlertDataJob($pack))->onQueue('default');
         }, 0);
         $mqtt->subscribe('mc/data', function (string $topic, string $message) use ($mqtt) {
             //echo $message . "\n";
@@ -62,26 +49,23 @@ class ConsumerCommand extends Command
             $pack= base64_encode($message);
             dispatch(new SaveMicrocontrollerDataJob($pack, false))->onQueue('spot');
         }, 2);
-        $mqtt->subscribe('mc/alert', function (string $topic, string $message) {
+        //$mqtt->subscribe('mc/alert', function (string $topic, string $message) {
             //echo "msj ALERTA = ".$message."\n";
-            dispatch(new SaveMicrocontrollerDataJob($message, true))->onQueue('spot');
-            dispatch(new SaveAlertDataJob($message))->onQueue('default');
-        }, 0);
-        $mqtt->subscribe('mc/config/#', function (string $topic, string $message) {
-            //echo "msj c = ".$message."\n";
+          //  dispatch(new SaveMicrocontrollerDataJob($message, true))->onQueue('spot');
+        //    dispatch(new SaveAlertDataJob($message))->onQueue('default');
+      //  }, 0);
 
-        }, 0);
-        $mqtt->subscribe('mc/ack', function (string $topic, string $message) {
+        //$mqtt->subscribe('mc/ack', function (string $topic, string $message) {
             //echo $message . "\n";
-            $json = json_decode($message, true);
-            if ($json != null) {
-                if (array_key_exists('config_get', $json)) {
+            //$json = json_decode($message, true);
+            //if ($json != null) {
+             //   if (array_key_exists('config_get', $json)) {
                     // dispatch(new SetConfigJob($json))->onQueue('spot');
-                } elseif (array_key_exists('frame_save', $json)) {
+           //     } elseif (array_key_exists('frame_save', $json)) {
                     // dispatch(new SetClientStopUnpackDataJob($json))->onQueue('spot');
-                }
-            }
-        }, 2);
+         //       }
+       //     }
+     //   }, 2);
         $mqtt->loop();
     }
 }
