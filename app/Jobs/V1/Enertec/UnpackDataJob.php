561e0c708ae03ffee56ee7b0330353e770af58d0
SneiderFuentes <s.fuentes@enerteclatam.com>
2024-02-20 08:05:15 -0500

    test

--- app/Jobs/V1/Enertec/UnpackDataJob.php
+++ app/Jobs/V1/Enertec/UnpackDataJob.php
@@ -165,7 +165,7 @@ class UnpackDataJob implements ShouldQueue
                         //dispatch(new JsonEdit($this->item->id, true))->onQueue($this->queue);
                         //}
                     } else {
-                        $this->item->delete();
+                        $this->item->forceDelete();
                     }
                 } else {
                     $this->item->forceDelete();
